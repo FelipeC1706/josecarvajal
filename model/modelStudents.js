89ad9211e7da7e69e0f9f503301237484029d284
FelipeC1706 <jcarvajal@uniempresarial.edu.co>
2023-08-14 22:24:25 -0500

    Modificar

---
+++ model/modelStudents.js
@@ -0,0 +1,42 @@
+const data= [
+
+    {
+
+        "name": "Jose felipe ",
+        "lastName":"Carvajal Campos",
+        "age":"35"
+
+    },
+
+    {
+
+        "name": "Kellyn Johanna",
+        "lastName":"Delgado Jaimes",
+        "age":"22"
+
+    },
+
+    {
+
+        "name": "Glenda",
+        "lastName":"Careli",
+        "age":"28"
+
+    },
+
+    {
+
+        "name": "Milton Andres",
+        "lastName":"Carvajal Peña",
+        "age":"43"
+
+    },
+
+];
+
+
+export{
+
+    data as students
+
+}
